5d95eb4ae809919d6ef199d7d24ccfc5724087ff
dhruvilrpatil <dhruvilrpatil@gmail.com>
2026-09-04 01:15:02 +0530

    Fix missing imports in dashboard_screen.dart

--- lib/features/dashboard/screens/dashboard_screen.dart
+++ lib/features/dashboard/screens/dashboard_screen.dart
@@ -1,4 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import '../../../core/theme/app_colors.dart';
+import '../../../core/theme/app_text_styles.dart';
+import '../../../core/constants/app_constants.dart';
+import '../../../shared/models/models.dart';
 import '../../../shared/providers/auth_providers.dart';
 
 class DashboardScreen extends ConsumerWidget {
